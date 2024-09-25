c6aef2efc20eaefddd5e6a33df0e647c1a298653
Marvin <88102362+marvinobig@users.noreply.github.com>
2024-09-25 11:57:42 +0100

    add code files for two completed days

---
+++ band-name-generator.py
@@ -0,0 +1,6 @@
+print("Hello!!, Welcome to Band Name Generator")
+
+userCity = input("What city did you grow up in?\n")
+userPetName = input("What would the name of your first pet be?\n")
+
+print(f"Your Band Name: {userCity.capitalize()} {userPetName.capitalize()}")
