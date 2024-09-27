a7f924f07d2db1479beb2f7cafbc2c1deea49b1d
Marvin <88102362+marvinobig@users.noreply.github.com>
2024-09-27 15:15:35 +0100

    Add files via upload

--- rps.py
+++ rps.py
@@ -26,8 +26,37 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
 choices = [rock, paper, scissors]
+userChoice = int(input("What's your play? (0 = rock, 1 = paper, 2 = scissors)"))
 computersChoice = random.randint(0, len(choices) - 1)
 
-print(f"The computer chose: \n{choices[computersChoice]}")
+if userChoice == computersChoice:
+    print(f"""
+You chose: \n{choices[userChoice]}
+The computer chose: \n{choices[computersChoice]}\n
+Result: Draw
+""")
+elif userChoice == 0 and computersChoice == 2:
+    print(f"""
+You chose: \n{choices[userChoice]}
+The computer chose: \n{choices[computersChoice]}\n
+Result: You Win!!
+""")
+elif userChoice == 2 and computersChoice == 1:
+    print(f"""
+You chose: \n{choices[userChoice]}
+The computer chose: \n{choices[computersChoice]}\n
+Result: You Win!!
+""")
+elif userChoice == 1 and computersChoice == 0:
+    print(f"""
+You chose: \n{choices[userChoice]}
+The computer chose: \n{choices[computersChoice]}\n
+Result: You Win!!
+""")
+else:
+    print(f"""
+You chose: \n{choices[userChoice]}
+The computer chose: \n{choices[computersChoice]}\n
+Result: You Lose. Game Over!!
+""")
