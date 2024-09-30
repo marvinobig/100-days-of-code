27002155c4bd28922db44db2165210f7c59d911e
Marvin <88102362+marvinobig@users.noreply.github.com>
2024-09-30 23:21:49 +0100

    update day 7 code

--- temperature-conversion-tool.py
+++ temperature-conversion-tool.py
@@ -1,7 +1,7 @@
 # Project: "Temperature Conversion Tool"
 #
 # Objective:
-# Build a basic temperature conversion tool that converts between Celsius, Fahrenheit, and Kelvin.
+# Build a basic temperature conversion tool that converts between Celsius, Fahrenheit and vice versa.
 #
 # Requirements:
 # Ask the user which scale they want to convert the temperature to (celsius to fahrenheit, fahrenheit to celsius).
@@ -9,15 +9,10 @@
 # Use variables to store the temperature (float) and scale (string).
 # Ensure proper input validation to check for valid temperature values and scales.
 # Based on the input, check the current temperature scale and convert it to the requested scale.
-# Use conditional statements to handle different conversions (Celsius to Fahrenheit, Fahrenheit to Kelvin, etc.).
+# Use conditional statements to handle different conversions (Celsius to Fahrenheit, etc.).
 # Allow the user to perform multiple conversions until they choose to exit the program.
 # After each conversion, display the result and ask if the user wants to perform another conversion.
-# Check which conversion the user wants and whether the input values are valid (e.g., the temperature isn't below absolute zero for Kelvin).
-#
-# Example Flow:
-# The user inputs a temperature in Celsius, and the system converts it to Fahrenheit.
-# The user can then convert another temperature, say from Kelvin to Celsius.
-# The program loops until the user decides to exit.
+
 
 def celsius_to_fahrenheit(temp):
     fahrenheit = (temp * 9 / 5) + 32
@@ -33,7 +28,7 @@ def check_for_float_number(string):
     try:
         float(string)
         return True
-    except TypeError:
+    except ValueError:
         return False
 
 available_conversions = ['celsius', 'fahrenheit']
@@ -62,7 +57,7 @@ while not stop:
     else:
         print(f"Fahrenheit: {float(user_temp_value)} to Celsius: {fahrenheit_to_celsius(float(user_temp_value))}")
 
-    user_confirmation = input("Would you like to convert another value? ").lower()
+    user_confirmation = input("Would you like to convert another value [no/yes]? ").lower()
 
     stop = True if user_confirmation == "no" else False
 
