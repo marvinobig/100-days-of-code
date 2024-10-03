f564049905b2666cfe15100e93395a4dd3e0492b
Marvin <88102362+marvinobig@users.noreply.github.com>
2024-10-03 11:43:11 +0100

    refactor to use recursion

--- sum-of-digits.py
+++ sum-of-digits.py
@@ -2,27 +2,15 @@
 # bonus - if the sum of the digits is greater than 9, continue summing the digits of the result until you get a single-digit number
 
 def sum_of_digits(n):
-    nums1 = [int(char) for char in str(n)]
-    count1 = 0
-    count2 = 0
-    total1 = 0
-    total2 = 0
+    nums_list = [int(char) for char in str(n)]
+    total = 0
 
-    while count1 < len(nums1):
-        total1 += nums1[count1]
-        count1 += 1
+    for num in nums_list:
+        total += num
 
-    if total1 > 9:
-        nums2 = [int(char) for char in str(total1)]
+    if total > 9:
+        return sum_of_digits(total)
 
-        while count2 < len(nums2):
-            total2 += nums2[count2]
-            count2 += 1
+    return total
 
-        return total2
-
-    return total1
-
-print(sum_of_digits(456))
-
-# can improve this with recursion
+print(sum_of_digits(99999))
